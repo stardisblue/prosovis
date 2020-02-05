7385cd684271a8dbde8e18d0916b02a3f57dce0c
Fati Chen <chen@lirmm.fr>
2020-02-05 10:28:09 +0100

    feat: better colors

--- src/context/ColorContext.ts
+++ src/context/ColorContext.ts
@@ -8,20 +8,30 @@ type ColorContextProps = {
 };
 
 export const ColorContext = React.createContext<ColorContextProps>({} as any);
+
+// TODO http://colorbrewer2.org/#type=qualitative&scheme=Paired&n=7
 export const useColorContext = function() {
   return useMemo(function() {
     const color = d3
       .scaleOrdinal<AnyEvent['kind'] | string, string>()
       .domain([
-        'Birth',
-        'Death',
-        'Education',
         'PassageExamen',
+        'Birth',
+        'Education',
+        'Retirement',
         'SuspensionActivity',
-        'ObtainQualification',
-        'Retirement'
+        'Death',
+        'ObtainQualification'
       ])
-      .range((d3 as any).schemeTableau10);
+      .range([
+        '#a6cee3',
+        '#1f78b4',
+        '#b2df8a',
+        '#33a02c',
+        '#fb9a99',
+        '#e31a1c',
+        '#fdbf6f'
+      ]);
 
     const border = d3
       .scaleOrdinal<AnyEvent['kind'] | string, string>()
