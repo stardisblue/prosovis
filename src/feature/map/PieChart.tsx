2da5a9392fb6874ae9bbf354fd2d759b666a7b7a
stardisblue <stardisblue@gmail.com>
2020-03-31 18:38:20 +0200

    opti: improving how piechart are dimmed

--- src/feature/map/PieChart.tsx
+++ src/feature/map/PieChart.tsx
@@ -3,34 +3,9 @@ import React, { useMemo } from 'react';
 import * as d3 from 'd3';
 import { selectSwitchColor } from '../../selectors/switch';
 import { superSelectionAsMap } from '../../selectors/superHighlights';
-import { createSelector } from '@reduxjs/toolkit';
 import useHoverHighlight from '../../hooks/useHoverHighlight';
 import { useSelector } from 'react-redux';
 import { useClickSelect } from '../../hooks/useClick';
-
-const selectDimmedColor = createSelector(selectSwitchColor, color => {
-  const domain = color.domain();
-  const range = _.map(domain, (d: any) => color(d));
-
-  return d3
-    .scaleOrdinal<string | number, string>()
-    .domain(
-      _.concat(
-        domain,
-        _.map(domain, i => 'd:' + i)
-      )
-    )
-    .range(
-      _.concat(
-        range,
-        _.map(range, d => {
-          const cl = d3.color(d)!;
-          cl.opacity = 0.3;
-          return cl.toString();
-        })
-      )
-    );
-});
 
 export const PieChart: React.FC<{
   radius: number;
@@ -55,7 +30,7 @@ export const PieChart: React.FC<{
   );
 
   return (
-    <g stroke="white">
+    <g stroke="white" pointerEvents="initial">
       {_.map(arcs, a => (
         <PiePath key={a.data[0]} a={a} arc={arc} />
       ))}
@@ -68,15 +43,17 @@ export const PiePath: React.FC<{
   a: d3.PieArcDatum<[string, any[]]>;
   arc: d3.Arc<any, d3.PieArcDatum<[string, any[]]>>;
 }> = function({ a, arc }) {
-  const color = useSelector(selectDimmedColor);
+  const color = useSelector(selectSwitchColor);
   const selected = useSelector(superSelectionAsMap);
 
-  const fill = useMemo(() => {
-    return _.isEmpty(selected) ||
+  const opacity = useMemo(
+    () =>
+      _.isEmpty(selected) ||
       _.some(a.data[1], ({ id }) => selected[id] !== undefined)
-      ? color(a.data[0])
-      : color('d:' + a.data[0]);
-  }, [a.data, selected, color]);
+        ? 1
+        : 0.3,
+    [a.data, selected, color]
+  );
 
   const interactive = useMemo(
     () => _.map(a.data[1], ({ id }) => ({ id, kind: 'Event' })),
@@ -85,7 +62,8 @@ export const PiePath: React.FC<{
 
   return (
     <path
-      fill={fill}
+      fill={color(a.data[0])}
+      opacity={opacity}
       d={arc(a)!}
       {...useClickSelect(interactive)}
       {...useHoverHighlight(interactive)}
