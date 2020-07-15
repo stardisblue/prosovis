c59e9deb999ef00e2f3f0078bf73315d4ae5f69b
Fati Chen <chen@lirmm.fr>
2020-07-15 11:13:12 +0200

    fixes #11

--- src/feature/global-graph/DetailsMenuEvents.tsx
+++ src/feature/global-graph/DetailsMenuEvents.tsx
@@ -2,5 +2,35 @@ import React from 'react';
 import { AnyEvent } from '../../data';
 import _ from 'lodash';
 export function DetailsMenuEvents({ events }: { events: AnyEvent[] }) {
-  return <div>{_.map(events, 'label')}</div>;
+  const grouped = _(events)
+    .sortBy('datation[0].clean_date')
+    .transform((acc, curr) => {
+      const prev = _.last(acc);
+      if (prev?.kind === curr.kind) {
+        prev.events.push(curr);
+      } else {
+        acc.push({ kind: curr.kind, events: [curr] });
+      }
+    }, [] as { kind: string; events: AnyEvent[] }[])
+    .value();
+  return (
+    <div>
+      {_.map(grouped, ({ kind, events }) => (
+        <DetailsMenuEvent key={events[0].id} kind={kind} events={events} />
+      ))}
+    </div>
+  );
+}
+function DetailsMenuEvent({
+  kind,
+  events,
+}: {
+  kind: string;
+  events: AnyEvent[];
+}) {
+  return (
+    <div>
+      {events.length} x {kind}
+    </div>
+  );
 }
