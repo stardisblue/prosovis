7eedfece5cfea289cfba152ffff231513a436cc6
Fati Chen <chen@lirmm.fr>
2020-08-17 17:46:09 +0200

    fixes #22

--- src/feature/global-graph/DetailsMenu.tsx
+++ src/feature/global-graph/DetailsMenu.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components/macro';
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import DetailsMenuContext from './DetailsMenuContext';
 import { Spring, animated } from 'react-spring/renderprops';
 import { DetailsMenuContent } from './DetailsMenuContent';
 import { stopEventPropagation } from '../../hooks/useClick';
+import { Ressource } from '../../data';
 
-const StyledDiv = styled.div`
+const StyledDetailsOnWheelDiv = styled(DetailsOnWheelDiv)`
   width: 100%;
   height: 100%;
   overflow-y: scroll;
@@ -16,6 +17,36 @@ const StyledDiv = styled.div`
   box-shadow: 0 0 3px gray;
   padding: 0 1em;
 `;
+
+function DetailsOnWheelDiv({
+  actor,
+  className,
+}: {
+  actor: Ressource;
+  className?: string;
+}) {
+  const ref = useRef<HTMLDivElement>(null as any);
+
+  useEffect(() => {
+    /* react onWheel event is attached directly to top element event listener, 
+    aka: document.addEventListeners
+    as we don't want EasyPZ to react to wheel events in this case, we are
+    forced to use legacy eventlistener, hence the effect
+     */
+    const rf = ref.current;
+    rf.addEventListener('wheel', stopEventPropagation);
+
+    return () => {
+      rf.removeEventListener('wheel', stopEventPropagation);
+    };
+  }, []);
+
+  return (
+    <div className={className} ref={ref}>
+      <DetailsMenuContent actor={actor} />
+    </div>
+  );
+}
 
 function DetailsMenu() {
   const { menuTarget } = useContext(DetailsMenuContext);
@@ -35,11 +66,8 @@ function DetailsMenu() {
             width="250"
             height="300"
             onClick={stopEventPropagation}
-            onWheel={stopEventPropagation}
           >
-            <StyledDiv>
-              <DetailsMenuContent actor={menuTarget.actor} />
-            </StyledDiv>
+            <StyledDetailsOnWheelDiv actor={menuTarget.actor} />
           </animated.foreignObject>
         )}
       </Spring>
