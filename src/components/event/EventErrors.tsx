9cd3dc76af166d599b9000eec5456504703136de
Fati Chen <chen@lirmm.fr>
2020-09-29 12:17:39 +0200

    fix #31, better error icons

--- src/components/event/EventErrors.tsx
+++ src/components/event/EventErrors.tsx
@@ -1,22 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import { SipError } from '../../data/sip-models';
-import { IconSpacer, IconSpacerPointer } from '../ui/IconSpacer';
-import {
-  AlertIcon,
-  IconProps,
-  InfoIcon,
-  XCircleFillIcon,
-  XCircleIcon,
-  XIcon,
-} from '@primer/octicons-react';
+import { IconSpacerPointer } from '../ui/IconSpacer';
+import { IconProps, XCircleFillIcon, XIcon } from '@primer/octicons-react';
 import { compact, groupBy } from 'lodash/fp';
-import { blue, red, orange, darkgray } from '../ui/colors';
+import { blue, red, orange } from '../ui/colors';
 import styled, { StyledComponent } from 'styled-components/macro';
 import AlertFillIcon from '../ui/icon/AlertFillIcon';
 import InfoFillIcon from '../ui/icon/InfoFillIcon';
 import { usePopper, useRefPopper } from '../ui/Popper';
 import { stopEventPropagation, useFlatClick } from '../../hooks/useClick';
-import { StyledFlex } from '../ui/Flex/styled-components';
 
 const SipErrorIcon = styled(XCircleFillIcon)`
   color: ${red};
@@ -27,13 +19,6 @@ const SipWarningIcon = styled(AlertFillIcon)`
 const SipInfoIcon = styled(InfoFillIcon)`
   color: ${blue};
 `;
-
-function plural(value: number, singular: string, plural: string) {
-  if (value === 0) return '';
-
-  if (value === 1) return singular;
-  else return `${value} ${plural}`;
-}
 
 const StyledDiv = styled.div`
   z-index: 9998;
@@ -54,14 +39,11 @@ const CloseButton = styled.div`
   top: 0;
 `;
 
-function getIcon(
-  errors: { [k in SipError['level']]?: SipError[] }
-): StyledComponent<React.FC<IconProps>, any, {}, never> {
-  if (errors.Error) return SipErrorIcon;
-  if (errors.Warning) return SipWarningIcon;
-  if (errors.Info) return SipInfoIcon;
+function plural(value: number, singular: string, plural: string) {
+  if (value === 0) return '';
 
-  throw new Error('Unreachable code');
+  if (value === 1) return singular;
+  else return `${value} ${plural}`;
 }
 
 function getErrorLabel(errors: { [k in SipError['level']]?: SipError[] }) {
@@ -72,35 +54,50 @@ function getErrorLabel(errors: { [k in SipError['level']]?: SipError[] }) {
   ]).join(', ');
 }
 
-/**
- * @param errors
- * @deprecated use simple string instead
- */
-export function getRichErrorLabel(
-  errors: { [k in SipError['level']]?: SipError[] }
-) {
-  const acc: (JSX.Element | number | string)[] = [];
-  if (errors.Error)
-    acc.push(<IconSpacer as={XCircleIcon} spaceRight />, errors.Error.length);
-  if (errors.Warning)
-    acc.push(
-      <IconSpacer as={AlertIcon} spaceLeft={acc.length > 0} spaceRight />,
-      errors.Warning.length
-    );
-  if (errors.Info)
-    acc.push(
-      <IconSpacer as={InfoIcon} spaceLeft={acc.length > 0} spaceRight />,
-      errors.Info.length
-    );
+function getErrorInfo(
+  errors: SipError[]
+): [StyledComponent<React.FC<IconProps>, any, {}, never>, string, string] {
+  const groupedErrors: { [k in SipError['level']]?: SipError[] } = groupBy(
+    'level',
+    errors
+  );
+  const label = getErrorLabel(groupedErrors);
 
-  return <StyledFlex>{acc}</StyledFlex>;
+  if (groupedErrors.Error) return [SipErrorIcon, red, label];
+  if (groupedErrors.Warning) return [SipWarningIcon, orange, label];
+  if (groupedErrors.Info) return [SipInfoIcon, blue, label];
+
+  throw new Error('Unreachable code');
 }
 
-const PilledIconSpacer = styled(IconSpacerPointer)<{ count: number }>`
+// /**
+//  * @param errors
+//  * @deprecated use simple string instead
+//  */
+// function getRichErrorLabel(
+//   errors: { [k in SipError['level']]?: SipError[] }
+// ) {
+//   const acc: (JSX.Element | number | string)[] = [];
+//   if (errors.Error)
+//     acc.push(<IconSpacer as={XCircleIcon} spaceRight />, errors.Error.length);
+//   if (errors.Warning)
+//     acc.push(
+//       <IconSpacer as={AlertIcon} spaceLeft={acc.length > 0} spaceRight />,
+//       errors.Warning.length
+//     );
+//   if (errors.Info)
+//     acc.push(
+//       <IconSpacer as={InfoIcon} spaceLeft={acc.length > 0} spaceRight />,
+//       errors.Info.length
+//     );
+
+//   return <StyledFlex>{acc}</StyledFlex>;
+// }
+
+const PilledIconSpacer = styled(IconSpacerPointer)`
   position: relative;
-  &:after {
-    content: '${({ count }) => (count > 9 ? '9+' : count)}';
-    background: ${darkgray};
+  &:before {
+    content: attr(data-count);
     border-radius: 999px;
     line-height: 1;
     font-size: 10px;
@@ -115,31 +112,34 @@ const PilledIconSpacer = styled(IconSpacerPointer)<{ count: number }>`
     padding: 0 2px;
   }
 `;
+
+const ColoredPilledIconSpacer = styled(PilledIconSpacer)<{ color: string }>`
+  &:before {
+    background-color: ${({ color }) => color};
+  }
+`;
+
 export const SimpleEventErrors: React.FC<{ errors: SipError[] }> = function ({
   errors,
 }) {
-  const groupedErrors: { [k in SipError['level']]?: SipError[] } = groupBy(
-    'level',
-    errors
-  );
-  const label = getErrorLabel(groupedErrors);
-  const Icon = getIcon(groupedErrors);
+  const [Icon, color, label] = getErrorInfo(errors);
 
   const [hint, $ref, showHint, hideHint] = useRefPopper<HTMLDivElement>(label);
 
   return (
     <>
-      <PilledIconSpacer
+      <ColoredPilledIconSpacer
         spaceLeft
         ref={$ref}
         onMouseEnter={showHint}
         onFocus={showHint}
         onMouseLeave={hideHint}
         onBlur={hideHint}
-        count={errors.length}
+        color={color}
+        data-count={errors.length}
       >
         <Icon aria-label={label} />
-      </PilledIconSpacer>
+      </ColoredPilledIconSpacer>
       {hint}
     </>
   );
@@ -150,16 +150,8 @@ export const EventErrors: React.FC<{ errors: SipError[] }> = function ({
 }) {
   const $ref = useRef<HTMLDivElement>(null as any);
 
-  const groupedErrors: { [k in SipError['level']]?: SipError[] } = groupBy(
-    'level',
-    errors
-  );
-  const label = getErrorLabel(groupedErrors);
-  const Icon = getIcon(groupedErrors);
-
   const [showContextMenu, setContextMenuState] = useState(false);
 
-  const [hint, showHint, hideHint] = usePopper($ref, label);
   const [details, showDetails, hideDetails] = usePopper(
     $ref,
     <DetailsMenu
@@ -188,10 +180,15 @@ export const EventErrors: React.FC<{ errors: SipError[] }> = function ({
     }
   }, [showContextMenu, showDetails, hideDetails]);
 
+  const [Icon, color, label] = useMemo(() => getErrorInfo(errors), [errors]);
+  const [hint, showHint, hideHint] = usePopper($ref, label);
+
   return (
     <>
-      <IconSpacerPointer
+      <ColoredPilledIconSpacer
         spaceLeft
+        color={color}
+        data-count={errors.length}
         ref={$ref}
         onMouseEnter={showHint}
         onFocus={showHint}
@@ -206,7 +203,7 @@ export const EventErrors: React.FC<{ errors: SipError[] }> = function ({
       >
         <Icon aria-label={label} />
         {details}
-      </IconSpacerPointer>
+      </ColoredPilledIconSpacer>
       {hint}
     </>
   );
@@ -214,7 +211,6 @@ export const EventErrors: React.FC<{ errors: SipError[] }> = function ({
   function handleContextMenu(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
     e.stopPropagation();
     e.preventDefault();
-    console.log('hello click');
     setContextMenuState((state) => !state);
   }
 };
