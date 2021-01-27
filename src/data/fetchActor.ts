6990b34500695c937a09629aa17f925e83a1019e
stardisblue <stardisblue@gmail.com>
2021-01-27 17:46:09 +0100

    hack for letting it work on htttps

--- src/data/fetchActor.ts
+++ src/data/fetchActor.ts
@@ -5,7 +5,8 @@ const axiosConfig = {
   timeout: 4000,
 };
 
-const baseurl = 'http://advanse.lirmm.fr/siprojuris/api/';
+const baseurl =
+  'https://cors-anywhere.herokuapp.com/http://advanse.lirmm.fr/siprojuris/api/';
 
 export function fetchActor(payload: PrimaryKey, config?: AxiosRequestConfig) {
   return axios.get(baseurl + 'actor/' + payload, axiosConfig);
